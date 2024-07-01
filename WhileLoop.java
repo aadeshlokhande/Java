83ea6486b769f4f1b1c785a3d7c57ff2ecec5899
Aadesh <aadeshlokhande11@gmail.com>
2024-07-01 14:09:40 +0530

    01 / 07 / 2024

--- WhileLoop.java
+++ WhileLoop.java
@@ -63,6 +63,7 @@
 
 // get 2 values from user and print addition till answer is 0(zero)
 
+// ****************  *****************
 // get values from user till 0. if he enter zero then print sum of all previous values
 // enter a number = 4 
 // enter a number = 6
@@ -71,8 +72,52 @@
 // enter a number = 0
 // answer = 20
 
+// import java.util.Scanner;
+
+// public class WhileLoop
+// {
+//     public static void main(String[] args)
+//     {
+//         int sum = 0,a=5; 
+//         Scanner scanner = new Scanner(System.in);
+
+//         while(a!=0)
+//         {
+//             System.out.print("Enter a number = ");
+//             a = scanner.nextInt();
+//             sum = sum + a;
+//         }
+
+//         System.out.println("total sum ="+sum);
+//         scanner.close();
+//     }
+// }
+// ****************  *****************
+
 // factorial
 // 5! = 5x4x3x2x1 = 120 
 
+
+// ****************  *****************
 // biryani = 120
 
+// import java.util.Scanner;
+
+// public class WhileLoop
+// {
+//     public static void main(String[] args)
+//     {
+//         int numPlate = 0, con = 1, biryani = 120;
+//         Scanner scanner = new Scanner(System.in);
+//         while(con==1)
+//         {
+//             System.out.print("do you want biryani 1/0 = ");
+//             con = scanner.nextInt();
+//             if(con==1)
+//                 ++numPlate;
+//         }
+//         con = numPlate * biryani;
+//         System.out.println("your total bill is "+con);
+//         scanner.close();
+//     }
+// }
