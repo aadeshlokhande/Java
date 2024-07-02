9ad464a97af66c30a74c277506e7283b8e090a3b
Aadesh <aadeshlokhande11@gmail.com>
2024-07-02 13:05:23 +0530

    02 / 07 / 2024

---
+++ Dowhile.java
@@ -0,0 +1,33 @@
+// Do while 
+
+
+// while(condition)
+// {
+//     code;
+// }
+
+
+// do
+// {
+//     code;
+// }while(condition);
+
+
+
+// import java.util.Scanner;
+
+// public class Dowhile
+// {
+//     public static void main(String[] args)
+//     {
+//         int num;
+//         Scanner scanner = new Scanner(System.in);
+//         do
+//         {
+//             System.out.print("enter a -ve number = ");
+//             num = scanner.nextInt();
+//         }while(num>=0);
+//         System.out.println("thank you");
+//         scanner.close();
+//     }
+// }
---
+++ SwitchCase.java
@@ -0,0 +1,157 @@
+// Switch Case
+
+// switch (value)
+// {
+//     case value1:
+//         code;
+    
+//     case value2:
+//         code;
+    
+//     case value3:
+//         code;
+
+//     default:
+//         code;
+
+// }
+
+
+// import java.util.Scanner;
+
+// public class SwitchCase
+// {
+//     public static void main(String[] args)
+//     {
+//         int num;
+//         Scanner scanner = new Scanner(System.in);
+//         System.out.print("Enter a number = ");
+//         num = scanner.nextInt();
+
+//         switch(num)
+//         {
+//             case 1:
+//                 System.out.println("ONE");
+//                 break;
+
+//             case 2:
+//                 System.out.println("TWO");
+//                 break;
+            
+//             case 30:
+//                 System.out.println("THREE");
+//                 break;
+            
+//             case 4:
+//                 System.out.println("FOUR");
+//                 break;
+            
+//             default:
+//                 System.out.println("invalid number");
+//                 break;
+
+//         }
+//         scanner.close();
+//     }
+// }
+
+
+
+// import java.util.Scanner;
+
+// public class SwitchCase
+// {
+//     public static void main(String[] args)
+//     {
+//         int a,b,op;
+//         Scanner scanner = new Scanner(System.in);
+//         System.out.print("Enter a number = ");
+//         a = scanner.nextInt();
+//         System.out.print("Enter a number = ");
+//         b = scanner.nextInt();
+
+//         System.out.println("press 1 for Add");
+//         System.out.println("press 2 for Sub");
+//         System.out.println("press 3 for Multi");
+//         System.out.println("press 4 for Div");
+        
+//         System.out.print("Enter your choice = ");
+//         op = scanner.nextInt();
+
+//         switch(op)
+//         {
+//             case 1:
+//                 System.out.println(a+b);
+//                 break;
+
+//             case 2:
+//                 System.out.println(a-b);
+//                 break;
+            
+//             case 3:
+//                 System.out.println(a*b);
+//                 break;
+            
+//             case 4:
+//                 System.out.println(a/b);
+//                 break;
+            
+//             default:
+//                 System.out.println("invalid choice");
+//                 break;
+
+//         }
+//         scanner.close();
+//     }
+// }
+
+
+
+
+// import java.util.Scanner;
+
+// public class SwitchCase
+// {
+//     public static void main(String[] args)
+//     {
+//         int a,b,op;
+//         Scanner scanner = new Scanner(System.in);
+//         System.out.print("Enter a number = ");
+//         a = scanner.nextInt();
+//         System.out.print("Enter a number = ");
+//         b = scanner.nextInt();
+
+//         System.out.println("press 1 for Add");
+//         System.out.println("press 2 for Sub");
+//         System.out.println("press 3 for Multi");
+//         System.out.println("press 4 for Div");
+        
+//         System.out.print("Enter your choice = ");
+//         op = scanner.nextInt();
+
+//         switch(op)
+//         {
+//             case 1:
+//                 System.out.println(a+b);
+//                 break;
+
+//             case 2:
+//                 System.out.println(a-b);
+//                 break;
+            
+//             case 3:
+//                 System.out.println(a*b);
+//                 break;
+            
+//             case 4:
+//                 System.out.println(a/b);
+//                 break;
+            
+//             default:
+//                 System.out.println("invalid choice");
+//                 break;
+
+//         }
+//         scanner.close();
+//     }
+// }
