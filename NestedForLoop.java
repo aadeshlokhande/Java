c7c1a8218477dc7c6b1727419f7c32daca32c4d9
Aadesh <aadeshlokhande11@gmail.com>
2024-07-02 12:28:03 +0530

    02 / 07 / 2024

--- NestedForLoop.java
+++ NestedForLoop.java
@@ -537,3 +537,22 @@
 // 1. hello 
 // ```code```
 // `code`
+
+
+
+
+// public class NestedForLoop
+//  {
+//      public static void main(String[] args) {
+//          for (int i = 1; i <= 5; i++) {
+//              for (int j = 1; j <= 5; j++) {
+//                  if (i <= j) 
+//                  {
+//                      System.out.print(" *");
+//                  } else
+//                      System.out.print(" ");
+//              }
+//              System.out.print("\n");
+//          }
+//      }
+//  }
