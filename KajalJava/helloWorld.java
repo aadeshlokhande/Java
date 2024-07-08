905db488d887cfd85defaafcbcad85cf17d136f8
Aadesh <aadeshlokhande11@gmail.com>
2024-07-08 09:51:41 +0530

    08 / 07 / 2024

---
+++ KajalJava/helloWorld.java
@@ -0,0 +1,20 @@
+// code ----> compile ----> bytecode -----> interprete ----> output
+// code ---> interpret ---> output
+
+// hello.java ---> hello.class 
+
+// JDK ---> java development kit 
+// IDE -----> vscode, intelij 
+
+public class helloWorld
+{
+    public static void main(String[] args)
+    {
+        System.out.println("hello");
+    }
+}
+if = 10 
+// whitespace ---> blank spaces
+// TOKENS ---> smallest element
+// semicolon ---> full stop ----> statement terminator
+// keywords ---> reserve words
