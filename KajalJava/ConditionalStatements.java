30f7281d7cc207ad5e02d9c203abae20308e9112
Aadesh <aadeshlokhande11@gmail.com>
2024-07-13 17:05:00 +0530

    13 / 07 / 2024

---
+++ KajalJava/ConditionalStatements.java
@@ -0,0 +1,187 @@
+// Conditional Statements
+
+// if
+
+// a == 0 -----> false 
+// 000000
+
+// a != 0 -------> True 
+// 101---->
+
+// int a = 5;
+
+// if(a)
+// {
+//     print(something)
+// }
+
+// syntax
+// if(condition)
+// {
+//     code;
+// }
+
+
+
+// public class ConditionalStatements
+// {
+//     public static void main(String[] args)
+//     {
+//         int a = 16;
+//         if(a<10)
+//         {
+//             System.out.println("True");
+//         }
+//     }
+// }
+
+
+// import java.util.Scanner;
+
+// public class ConditionalStatements
+// {
+//     public static void main(String[] args)
+//     {
+//         int age;
+//         Scanner scanner = new Scanner(System.in);
+//         System.out.print("Enter your age = ");
+//         age = scanner.nextInt();
+//         if(age>18)
+//         {
+//             System.out.print("Adult ");
+//         }
+
+//         System.out.println("Boy");
+        
+//         scanner.close();
+//     }
+// }
+
+
+
+// if else
+// if(condition)
+// {
+//     code;
+// }
+// else
+// {
+//     code;
+// }
+
+// import java.util.Scanner;
+
+// public class ConditionalStatements
+// {
+//     public static void main(String[] args)
+//     {
+//         int age;
+//         Scanner scanner = new Scanner(System.in);
+//         System.out.print("enter a age = ");
+//         age = scanner.nextInt();
+
+//         if(age>=18)
+//         {
+//             System.out.println("you can vote");
+//         }
+//         else
+//         {
+//             System.out.println("you can't vote");
+//         }
+//         scanner.close();
+//     }
+// }
+
+// import java.util.Scanner;
+
+// public class ConditionalStatements
+// {
+//     public static void main(String[] args)
+//     {
+//         int num;
+//         Scanner scanner = new Scanner(System.in);
+//         System.out.print("enter a number = ");
+//         num = scanner.nextInt();
+//         if(num>0)
+//         {
+//             System.out.println("+ve number");
+//         }
+//         else 
+//         {
+//             System.out.println("-ve number");
+//         }
+//         scanner.close();
+//     }
+// }
+
+
+// import java.util.Scanner;
+
+// public class ConditionalStatements
+// {
+//     public static void main(String[] args)
+//     {
+//         int num;
+//         Scanner scanner = new Scanner(System.in);
+//         System.out.print("enter a number =");
+//         num = scanner.nextInt();
+//         if(num%2 == 0)
+//         {
+//             System.out.println("even number");
+//         }
+//         else
+//         {
+//             System.out.println("ODD number");
+//         }
+//         scanner.close();
+//     }
+// }
+
+// 10 / 2 = 5 
+// 10 % 2 = 0
+
+// 13 / 2 = 6
+// 13 % 2 = 1
+
+// *******************************************
+// **************** Homework *****************
+// *******************************************
+// input
+// dogs = 6 
+// number of bis = 20
+
+// output
+// eat = 3 
+// remain = 2 
+
+
+// get seconds from user and convert it into hr, min, sec. 
+// enter a sec = 4000
+// hr = 1 
+// min = 6 
+// sec = 40 
+// 1 : 6 : 40
+
+
+// get amount from user and convert it into minimun notes and coins 
+// enter a amount = 8768
+// note2000 = 4 
+// note500 = 1 
+// note200 = 1 
+// note100 = 0
+// note50 = 1 
+// note20 = 0 
+// note10 = 1 
+// coin5 = 1 
+// coin2 = 1
+// coin1 = 1
+
+// swap 2 number 
+
+// get age from user and check its age is valid or not
+// enter a age = 100
+
+// get year from user and check its leap year or not 
+// get value from user and check its divisible by 7 or not 
+
+// get percentage from user and check he's pass or fail
