324047214476959fe05157274e36603579bfef2b
Aadesh <aadeshlokhande11@gmail.com>
2024-07-25 10:09:19 +0530

    25 / 07 / 2024

---
+++ KajalJava/whileLoop.java
@@ -0,0 +1,86 @@
+// whileLoop
+// while(condition)
+// {
+//     code;
+// }
+
+
+// import java.util.Scanner;
+
+// public class whileLoop
+// {
+//     public static void main(String[] args)
+//     {
+//         int a = 1;
+//         while(a<=10)
+//         {
+//             System.out.print("Hello ");
+//         }
+//     }
+// }
+
+
+
+// import java.util.Scanner;
+
+// public class whileLoop
+// {
+//     public static void main(String[] args)
+//     {
+//         int a,b,c,con = 1;
+
+//         Scanner scanner = new Scanner(System.in);
+//         while(con==1)
+//         {
+//             System.out.print("Enter a number = ");
+//             a = scanner.nextInt();
+//             System.out.print("Enter a number = ");
+//             b = scanner.nextInt();
+//             c = a + b;
+//             System.out.println(c);
+//             System.out.print("Do you want to continue 1/0 = ");
+//             con = scanner.nextInt();
+//         }
+//         scanner.close();
+//     }
+// }
+
+
+
+// ****************  *****************
+
+// enter a -ve number = 23
+// enter a -ve number = 5
+// enter a -ve number = 5
+// enter a -ve number = 87
+// enter a -ve number = -3
+// thank you 
+
+// import java.util.Scanner;
+
+// public class whileLoop
+// {
+//     public static void main(String[] args)
+//     {
+//         int a=4;
+//         Scanner scanner = new Scanner(System.in);
+//         while(a>=0)
+//         {
+//             System.out.print("Enter a -ve number = ");
+//             a = scanner.nextInt();
+//         }
+//         System.out.println("thank you");
+//         scanner.close();
+//     }
+// }
+
+
+
+// ======================================
+// *******************************************
+// **************** Homework *****************
+// *******************************************
+
+
+// get 2 values from user and print addition till answers is zero (0)
+// get values from user till he enter 0 then count all even number 
