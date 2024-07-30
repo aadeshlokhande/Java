d5dc00ea49a42c4cb5904205a313bec5d9367778
Aadesh <aadeshlokhande11@gmail.com>
2024-07-30 11:03:10 +0530

    30 / 07 / 2024

---
+++ KajalJava/Array.java
@@ -0,0 +1,85 @@
+// Array 
+
+// int a;
+
+
+
+// int[] a;
+// int []a;
+// int a[];
+
+// import java.util.Scanner;
+
+// public class Array
+// {
+//     public static void main(String[] args)
+//     {
+//         // int a[] = new int[10];
+//         // int a[];
+//         // a = new int[10];
+//         Scanner sc = new Scanner(System.in);
+//         // a[2] = 10;
+//         // a[4] = 20;
+//         // a[6] = 60;
+//         // System.out.print("Enter a number = ");
+//         // a[5] = sc.nextInt();
+//         // System.out.println(a[5]);
+
+//         // int a[] = {10,20,30,40,50,60};
+//         int a[] = new int[5];
+//         for(int i = 0; i<5; i++)
+//         {
+//             System.out.print("Enter a number = ");
+//             a[i] = sc.nextInt();
+//         }
+//         // System.out.println(a[2]);
+//         for(int i = 0; i<5; i++ )
+//         {
+//             System.out.println( "a["+i+"] = " + a[i] );
+//         }
+//     }
+// }
+
+// =======================================================================
+
+// import java.util.Scanner;
+
+// public class Array
+// {
+//     public static void main(String[] args)
+//     {
+//         int count = 0;
+//         int arr[] = new int[10];
+//         Scanner scanner = new Scanner(System.in);
+
+//         for(int i = 0; i<10; i++)
+//         {
+//             System.out.print("enter a number = ");
+//             arr[i] = scanner.nextInt();
+//         }
+
+//         for(int i = 0; i<10; i++)
+//         {
+//             if(arr[i] % 2 ==0)
+//             {
+//                 ++count;
+//                 System.out.println(arr[i]);
+//             }
+//         }
+        
+//         System.out.println("total even number = "+count);
+        
+//         scanner.close();
+//     }
+// }
+// ======================================================================
+
+
+// *******************************************
+// **************** Homework *****************
+// *******************************************
+
+// create a array and store the value
+// get 10 values from user and print total sum
+// get 10 values from user and count total off numbers
+// get 10 values from user and print them in reverse order
