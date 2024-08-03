d0b587dfa47de1b65b654ab1ce346d79b4795fa1
Aadesh <aadeshlokhande11@gmail.com>
2024-08-03 11:18:09 +0530

    03 / 08 / 2024

---
+++ KajalJava/Function.java
@@ -0,0 +1,40 @@
+// Function
+
+// returntype functionName(arguments)
+// {
+//     code;
+//     return value;
+// }
+
+
+
+
+public class Function
+{
+
+    static int sum(int a, int b)
+    {
+        int c;
+        c = a + b;
+        System.out.println("byebye");
+        return c;
+    }
+    
+    public static void main(String[] args)
+    {
+        System.out.println("Hello");
+        int a = sum(10,12);
+        System.out.println(a);
+        System.out.println("lol");
+    }
+    
+}
+// *******************************************
+// **************** Homework *****************
+// *******************************************
+
+// sub 
+// multi
+// div
+// mod 
+// area of circle
---
+++ KajalJava/typeOfFunction.java
@@ -0,0 +1,104 @@
+// typeOfFunction
+
+
+// with arguments with return 
+// import java.util.Scanner;
+// public class typeOfFunction
+// {
+
+//     static int square(int num)
+//     {
+//         int sq;
+//         sq = num * num;
+//         return sq;
+//     }
+//     public static void main(String[] args)
+//     {
+//         int a, num;
+// 		Scanner scanner = new Scanner(System.in);
+//         System.out.print("Enter a number = ");
+//         num = scanner.nextInt();
+
+//         a = square(num);
+//         System.out.println(a);
+
+//     }
+// }
+
+
+// **************** with arguments without return  *****************
+
+
+// public class typeOfFunction
+// {
+//     static void square(int a)
+//     {
+//         int ans;
+//         ans = a * a;
+//         System.out.println(ans);
+//     }
+
+//     public static void main(String[] args)
+//     {
+//         square(8);
+//         square(18);
+//         square(28);
+//     }
+// }
+
+// **************** without arguments with return  *****************
+
+// public class typeOfFunction
+// {
+//     static int square()
+//     {
+//         int a = 5,ans;
+//         ans = a * a;
+//         return ans;
+//     }
+
+//     public static void main(String[] args)
+//     {
+//         int num;
+//         num = square();
+//         System.out.println(num);
+//     }
+// }
+
+// **************** without arguments without return  *****************
+
+
+
+// import java.util.Scanner;
+// public class typeOfFunction
+// {
+
+//     static void square()
+//     {
+//         int a ,num;
+//         Scanner scanner = new Scanner(System.in);
+//         System.out.print("Enter a number = ");
+//         a = scanner.nextInt();
+//         num = a * a;
+//         System.out.println(num);
+        
+//     }
+//     public static void main(String[] args)
+//     {
+//         square();
+//         square();
+//         square();
+//         square();
+//         square();
+//     }
+// }
+
+// *******************************************
+// **************** Homework *****************
+// *******************************************
+
+// sub 
+// multi
+// div
+// mod 
+// area of circle
