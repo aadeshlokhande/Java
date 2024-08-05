32054d341f586e34fd7d8095e56b4dae82370d78
Aadesh <aadeshlokhande11@gmail.com>
2024-08-05 18:45:37 +0530

    05 / 08 / 2024

--- BreakContinue.java
+++ BreakContinue.java
@@ -29,3 +29,42 @@
 //         }
 //     }
 // }
+
+
+// =================================================
+
+import java.util.Scanner;
+
+public class BreakContinue
+{
+    public static void main(String[] args)
+    {
+        int num,a,count = 0;
+        
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number = ");
+        num = scanner.nextInt();
+        a = (int)num/2;
+
+        for(int i = 2; i<=a; i++)
+        {
+            for(int j = 2; j<=a; j++)
+            {
+                if(i*j == num)
+                {
+                    ++count;
+                }
+            }
+        }
+
+        if(count==0)
+        {
+            System.out.println("prime");
+        }
+        else 
+        {
+            System.out.println("not a prime");
+        }
+        scanner.close();
+    }
+}
