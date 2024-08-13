5e92e7dd9274928970384a0e3bf315db95c31e01
Aadesh <aadeshlokhande11@gmail.com>
2024-08-13 11:53:53 +0530

    13 / 08 / 2024

---
+++ Aug12.java
@@ -0,0 +1,63 @@
+
+// public class Aug12
+// {
+//     public static void main(String[] args)
+//     {
+//         System.out.print("");
+//     }
+// }
+
+
+// interface
+
+
+
+// interface carInterface
+// {
+//     void startCar();
+//     void Break();
+// }
+
+
+// class Car implements carInterface
+// {
+//     public void startCar()
+//     {
+//         System.out.println("Start car....");
+//     }
+//     public void Break()
+//     {
+//         System.out.println("Start car....");
+//     }
+
+// }
+
+
+// public class Aug12
+// {
+//     public static void main(String[] args)
+//     {
+//         Car nano = new Car();
+//         nano.startCar();
+
+//     }
+// }
+
+
+
+
+
+
+public class Aug12
+{
+    void printKaro(String a)
+    {
+        System.out.println(a);
+    }
+
+    public static void main(String[] args)
+    {
+        Aug12 ob = new Aug12();
+        ob.printKaro(10+2);
+    }
+}
